14fceac5ba029e05ff6c683293ac67345e5bb0cd
János Maginecz <wafleeee@gmail.com>
2015-11-19 09:34:55 +0100

    add setuptools

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='writingtools',
+    version=0.1,
+    description="Collection of tools for writing papers",
+    author='Janos Maginecz',
+    author_email='wafleeee@gmail.com',
+    url='https://github.com/wafle/writing-tools',
+    license='MIT',
+    packages=[],
+    install_requires=['json','requests_futures', 'beautifulsoup4'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={'console_scripts': [
+        'gcompare = writingtools.gcompare:main',
+        'synonym = writingtools.synonym:main'
+    ]}
+)
