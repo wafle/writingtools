76f4458b2dc3a57444bb8e242a626ba96a9c9a76
János Maginecz <wafleeee@gmail.com>
2016-07-15 22:36:44 +0200

    make gcompare take varargs

--- writingtools/gcompare.py
+++ writingtools/gcompare.py
@@ -1,35 +1,35 @@
 from requests_futures.sessions import FuturesSession
 import json
 
-def gcompare(query1, query2):
+def gcompare(*queries):
     session = FuturesSession()
-    query1 = '"' + query1 + '"'
-    query2 = '"' + query2 + '"'
-    term1 = session.get("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q={}".format(query1))
-    term2 = session.get("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q={}".format(query2))
+    results = []
+    for query in queries:
+        results.append(session.get('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="{}"'.format(query)))
+    results = [json.loads(r.result().text) for r in results]
+    for result in results:
+        if result["responseStatus"] == 403:
+            print("API limits reached")
+            exit(1)
+    arity_sum = 0
+    arities = []
+    for res in results:
+        if "estimatedResultCount" in res["responseData"]["cursor"]:
+            arity = int(res["responseData"]["cursor"]["estimatedResultCount"])
+            arities.append(arity)
+            arity_sum += arity
+        else:
+            arities.append(0)
 
-    term1_json = json.loads(term1.result().text)
-    term2_json = json.loads(term2.result().text)
-
-    if term1_json["responseStatus"] == 403 or term2_json["responseStatus"] == 403:
-        print("API limits reached")
-        exit(1)
-
-    term1_arity = int(term1_json["responseData"]["cursor"]["estimatedResultCount"])
-    term2_arity = int(term2_json["responseData"]["cursor"]["estimatedResultCount"])
-
-    sum = term1_arity + term2_arity
-    term1_percent = term1_arity / float(sum)
-    term2_percent = term2_arity / float(sum)
-
-    print("{} {:4.3f}: {}".format(query1, term1_percent, int(term1_percent * 50) * "#"))
-    print("{} {:4.3f}: {}".format(query2, term2_percent, int(term2_percent * 50) * "#"))
+    ratios = [ar / float(arity_sum) for ar in arities]
+    for (query, ratio, arity) in zip(queries, ratios, arities):
+        print("{} {:4.3f}: {}".format(query, ratio, int(ratio * 50) * "#"))
 
 def main():
     import sys
     arguments = sys.argv[1:]
-    if len(arguments) == 2:
-        gcompare(arguments[0], arguments[1])
+    if len(arguments) >= 2:
+        gcompare(*arguments)
     else:
         print("usage: gcompare <phrase1> <phrase2>")
 
